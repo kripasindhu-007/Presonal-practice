ccced65ed993b265dec8b128d3353a46871b9531
Kripa Sindhu <sindhukripa007@gmail.com>
2022-01-04 13:35:41 +0530

    CSE Lab problems

---
+++ FirstLab1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    printf("HELLO WORLD");
+    printf("\nKripa Sindhu");
+    printf("\n2101103");
+    printf("\nCSE Branch");
+    printf("\nBhagalpur,Bihar");
+    return 0;
+}
---
+++ FirstLab2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int num1,num2;
+    num1=23;//assigning value
+    num2=34;//assigning value
+    int sum=num1+num2;//addition
+    printf("%d",sum);//printing value
+    return 0;
+}
