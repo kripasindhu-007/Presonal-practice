43445b5a243a0abab11b5f5de789fc32dbd739c1
Kripa Sindhu <sindhukripa007@gmail.com>
2022-01-04 14:34:46 +0530

    Lab Problem

--- FirstLab2.c
+++ FirstLab2.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
 int main(){
     int num1,num2;
-    num1=23;//assigning value
-    num2=34;//assigning value
+    num1=21;//assigning value
+    num2=3;//assigning value
     int sum=num1+num2;//addition
-    printf("%d",sum);//printing value
+    int product=num1*num2;//multiplication
+    int subt=num1-num2;//subtraction
+    int div=num1/num2;//division
+    printf("%d\n",sum);//printing value
+    printf("%d\n",subt);//printing value
+    printf("%d\n",product);//printing value
+    printf("%d\n",div);//printing value
     return 0;
 }
