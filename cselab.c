6bdaafd6996fe9baa4b7ffd0aef4bbfe8a3a2a11
Kripa Sindhu <sindhukripa007@gmail.com>
2022-02-02 15:53:37 +0530

    verified

---
+++ cselab.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int marks=56;
+    
+    if(90<marks<=100)
+    printf("AA");
+    else if(80<marks<=90)
+    printf("AB");
+    else if(70<marks<=80)
+    printf("BB");
+    else if(60<marks<=70)
+    printf("BC");
+    else if(50<marks<=60)
+    printf("CC");
+    else if(40<marks<=50)
+    printf("AB");
+return 0;
+}
